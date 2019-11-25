f00b4e56218dfcce2a994dc08f6774b82c27cedf
umafl <umafl@yahoo.com>
2019-11-25 14:46:47 -0500

    Initial Commit

---
+++ src/main/java/org/launchcode/Depression/and/Anxiety/Space/DepressionAndAnxietySpaceApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.Depression.and.Anxiety.Space;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepressionAndAnxietySpaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepressionAndAnxietySpaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/Depression/and/Anxiety/Space/DepressionAndAnxietySpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.Depression.and.Anxiety.Space;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepressionAndAnxietySpaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
